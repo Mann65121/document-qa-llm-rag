483b5ecfd70d33c05333c563902046b621327c06
KuberChhabra1105 <Kuber.23fe10cse00331@muj.manipal.edu>
2026-02-12 20:15:31 +0900

    Integrated FAISS vector search with QA pipeline

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,18 @@
 from flask import Flask, request, jsonify
 from document_loader import load_document
+from embeddings import create_embeddings, model
+from vector_store import create_index, search_index
+
+faiss_index = None
+
+embeddings = create_embeddings(stored_chunks)
+faiss_index = create_index(embeddings)
+
+query_embedding = model.encode([question])
+indices = search_index(faiss_index, query_embedding)
+
+
+relevant_chunks = [stored_chunks[i] for i in indices]
 
 app = Flask(__name__)
 
